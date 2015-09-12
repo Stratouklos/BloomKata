97f16ba1eca6e9288bdde73469c29089b536a33c
Stratouklos <stratouklos@gmail.com>
2015-09-11 23:42:13 -0400

    First test is green

--- bloomKata/spellCheck.py
+++ bloomKata/spellCheck.py
@@ -1,1 +1,15 @@
 __author__ = 'exak'
+
+import sys
+
+
+def main(word):
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    print('Argument List:', str(sys.argv))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Please provide an argument')
+        exit(1)
+    main(sys.argv[1])
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,1 +1,5 @@
 __author__ = 'exak'
+from bloomKata import spellcheck
+
+def test_answer():
+    spellcheck.main('blarg')
