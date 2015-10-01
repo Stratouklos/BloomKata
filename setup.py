a87bea3c2467aeeff5eabb11cc5409e766e39f59
Stratouklos <stratouklos@gmail.com>
2015-09-30 20:55:44 -0400

    Add dependencies for bitstrings

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='Bloomkata',
       description='Bloom filter kata',
       author='Stratos Xakoustos',
       url='https://github.com/Stratouklos/BloomKata',
-      packages=['bloom_kata'],
+      packages=['bloom_kata'], requires=['bitstring']
       )
