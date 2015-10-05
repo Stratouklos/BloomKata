6be48fbc4f963595606b91ccf097671909a7df27
Stratouklos <stratouklos@gmail.com>
2015-10-04 23:16:40 -0400

    added the missing file

---
+++ bloom_kata/scaler.py
@@ -0,0 +1,29 @@
+import sys
+
+__author__ = 'exak'
+
+
+class MinimumScaleError(Exception):
+    pass
+
+
+def scale_down(array_of_hex_digits, max_size):
+    digits = check_size_and_get_digits(max_size)
+    if type(array_of_hex_digits) is None:
+        raise TypeError("Expected hexstring found None")
+    if len(array_of_hex_digits) is 0:
+        raise ValueError("The minimum hexstring length is 1")
+    index = int(array_of_hex_digits[0:digits], 16)
+    if index > max_size:
+        return max_size - index
+    else:
+        return index
+
+
+def check_size_and_get_digits(size):
+    digits = len(hex(size)) - 2
+    if digits <= 2:
+        raise ValueError("The minimum indexing size is 256")
+    if digits > 64:
+        raise ValueError("Maximum indexing size exceeded")
+    return digits
